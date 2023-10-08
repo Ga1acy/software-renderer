04ece6a9652b0e54983b36141764fdcaea19510e
galacy <galacy0420@gmail.com>
2023-10-08 12:06:33 +0800

    using light intensity(n * light_dir) to do flat-shading

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ const TGAColor white = TGAColor(255,255,255,255);
 const TGAColor red = TGAColor(255,0,0,255);
 const TGAColor blue = TGAColor(0,0,255,255);
 const TGAColor green = TGAColor(0,255,0,255);
-const int width = 200;
-const int height = 200;
+const int width = 800;
+const int height = 800;
 Model* model = nullptr;
 
 inline Vec2i lerp_in_y(int y, Vec2i t0, Vec2i t1) {
@@ -78,11 +78,33 @@ void triangle(Vec2i* vertices , TGAImage& image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    TGAImage frame(200, 200, TGAImage::RGB);
-    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)};
-    triangle(pts, frame, red);
-    frame.flip_vertically(); // to place the origin in the bottom left corner of the image
-    frame.write_tga_file("framebuffer.tga");
+
+    TGAImage image(width, height, TGAImage::RGB);
+    model = new Model("../obj/african_head.obj");
+    Vec2i* screen_coords;
+    Vec3f world_coords[3];
+    Vec3f light_dir(0,0,-1);
+
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            //get one of three vertex in a face
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x + 1.) * width / 2, (v.y + 1.) * height / 2);
+            world_coords[j] = v;
+        }
+        //the normal of a face
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * light_dir;
+
+        if (intensity > 0)
+            triangle(screen_coords,image,TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+
+    }
+    image.flip_vertically(); // to place the origin in the bottom left corner of the image
+    image.write_tga_file("flat-shading-with-light.tga");
+    delete model;
     return 0;
 }
 
