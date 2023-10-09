1349567709079ba3886f507419a4264b9cf14745
galacy <galacy0420@gmail.com>
2023-10-09 22:35:09 +0800

    add method to resolve vt

--- model.cpp
+++ model.cpp
@@ -20,21 +20,14 @@ Model::Model(const char *filename) : verts_(), faces_() {
             for (int i=0;i<3;i++) iss >> v[i];
             verts_.push_back(v);
         } else if (!line.compare(0, 2, "f ")) {
-            Face face;
-            int itrash, idx,texidx;
+            std::vector<int> f;
+            int itrash, idx;
             iss >> trash;
-            while (iss >> idx >> trash >> texidx >> trash >> itrash) {
+            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
                 idx--; // in wavefront obj all indices start at 1, not zero
-                texidx--;
-                face.vertexIndices.push_back(idx);
-                face.texcoordsIndices.push_back(texidx);
+                f.push_back(idx);
             }
-            faces_.push_back(face);
-        } else if(!line.compare(0, 3, "vt ")) {
-            iss >> trash >> trash;
-            Vec2f tc;
-            for (int i = 0; i < 2; i ++) iss >> tc[i];
-            texcoords_.push_back(tc);
+            faces_.push_back(f);
         }
     }
     std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
@@ -51,14 +44,10 @@ int Model::nfaces() {
     return (int)faces_.size();
 }
 
-Face Model::face(int idx) {
+std::vector<int> Model::face(int idx) {
     return faces_[idx];
 }
 
 Vec3f Model::vert(int i) {
     return verts_[i];
-}
-
-Vec2f& Model::getTexCoord(int idx) {
-    return texcoords_[idx];
 }
