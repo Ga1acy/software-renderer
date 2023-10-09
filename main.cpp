b5f91e26ca1603182cd3a4132adc9373f25bfc03
galacy <galacy0420@gmail.com>
2023-10-09 22:33:11 +0800

    flat-shading with texture

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ const int width = 800;
 const int height = 800;
 Model* model = nullptr;
 
+inline Vec2i lerp_in_y(int y, Vec2i t0, Vec2i t1) {
+    float alpha = (float)(y - t0.y) / (t1.y - t0.y);
+    Vec2i t = t0 + (t1 - t0)*alpha;
+    return t;
+}
+
 Vec3f computeBarycentric(const Vec3f* v, float x, float y) {
     float alpha = (x*(v[1].y - v[2].y) + (v[2].x - v[1].x)*y + v[1].x*v[2].y - v[2].x*v[1].y) / (v[0].x*(v[1].y- v[2].y) + (v[2].x - v[1].x)*v[0].y + v[1].x*v[2].y - v[2].x*v[1].y);
     float beta = (x*(v[2].y - v[0].y) + (v[0].x - v[2].x)*y + v[2].x*v[0].y - v[0].x*v[2].y) / (v[1].x*(v[2].y - v[0].y) + (v[0].x - v[2].x)*v[1].y + v[2].x*v[0].y - v[0].x*v[2].y);
@@ -22,15 +28,31 @@ Vec3f computeBarycentric(const Vec3f* v, float x, float y) {
     return {alpha,beta,gamma};
 }
 
-TGAColor getTextureColor(TGAImage& texture, float u, float v) {
-    u = std::max(0.0f, std::min(1.0f, u));
-    v = std::max(0.0f, std::min(1.0f, v));
 
-    float x = u * texture.get_width();
-    float y = v * texture.get_height();
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x,p0.y);
+        std::swap(p1.x,p1.y);
+        steep = true;
+    }
 
-    TGAColor color = texture.get(x,y);
-    return TGAColor(color[2], color[1], color[0], 255);
+    if (p0.x > p1.x) {
+        std::swap(p0.x,p1.x);
+        std::swap(p0.y,p1.y);
+    }
+
+    for (int x = p0.x; x <= p1.x; x++) {
+//        //liner interpolation
+//        //lerp(t,y0,y1) = t0 + x(t1-t0)
+        float t = (x - p0.x) /(float)(p1.x - p0.x);
+        int y = p0.y * (1.-t) + p1.y * t;
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+    }
 }
 
 void triangle(Vec3f* vertices , float* zBuffer, TGAImage& image, TGAColor color) {
@@ -65,72 +87,40 @@ void triangle(Vec3f* vertices , float* zBuffer, TGAImage& image, TGAColor color)
     }
 }
 
-//pts is the triangle's vertices, tex is triangle's vertices' correspond texture coordinates
-void triangleWithTexture(Vec3f* pts, Vec2f* tex, float* zBuffer, TGAImage& image, TGAImage& texture) {
-    Vec2f bounding_box_min(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    Vec2f bouding_box_max(-std::numeric_limits<float>::max(),-std::numeric_limits<float>::max());
-    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
-    //calculate the bouding box
-    for (int i = 0; i < 3; i++) {
-        bounding_box_min.x = std::max(0.f, std::min(pts[i].x , bounding_box_min.x));
-        bounding_box_min.y = std::max(0.f,std::min(pts[i].y, bounding_box_min.y));
-        bouding_box_max.x = std::min(clamp.x, std::max(bouding_box_max.x,pts[i].x));
-        bouding_box_max.y = std::min(clamp.y, std::max(bouding_box_max.y,pts[i].y));
-    }
-
-    Vec3f P;
-    for (P.x = bounding_box_min.x; P.x <= bouding_box_max.x; P.x++) {
-        for (P.y = bounding_box_min.y; P.y <= bouding_box_max.y; P.y++) {
-            Vec3f bc = computeBarycentric(pts, P.x, P.y);
-            if (bc.x < 0 || bc.y < 0 || bc.z < 0)
-                continue;
-            P.z = 0;
-            //using interpolate to get pixel's z value and u-v coordinates in texture
-            P.z = bc.x * pts[0].z + bc.y * pts[1].z + bc.z * pts[2].z;
-            Vec2f uv = bc.x * tex[0] + bc.y * tex[1] + bc.z * tex[2];
-
-            if (zBuffer[int(P.x + P.y * width)] < P.z) {
-                zBuffer[int(P.x + P.y * width)]  = P.z;
-                //v is from top to bottom, thus we use 1-uv.y to flip
-                image.set(P.x, P.y, getTextureColor(texture, uv.x, 1 - uv.y));
-            }
-        }
-    }
-}
-
 Vec3f world2screen(Vec3f v) {
     return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 int main(int argc, char** argv) {
+
     TGAImage image(width, height, TGAImage::RGB);
-    TGAImage texture;
     model = new Model("../obj/african_head.obj");
-    if (texture.read_tga_file("../obj/african_head_diffuse.tga")) {
-        std::cout << "texture successfully loaded" << std::endl;
-    } else {
-        std::cerr << "Failed to load texture" << std::endl;
-    }
 
     float* zBuffer = new float[width * height];
     //initialize zBuffer, when i < 0, automatically jump out for loop
     for (int i = width * height; i--; zBuffer[i] = - std::numeric_limits<float>::max());
+    Vec3f light_dir(0,0,-1);
+
     for (int i = 0; i < model->nfaces(); i++) {
-        Face face = model->face(i);
+        std::vector<int> face = model->face(i);
         Vec3f screen_coords[3], world_coords[3];
-        Vec2f tex_coords[3];
         for (int j = 0; j < 3; j++) {
             //get one of three vertex in a face
-            world_coords[j] = model->vert(face.vertexIndices[j]);
-            tex_coords[j] = model->getTexCoord(face.texcoordsIndices[j]);
+            world_coords[j] = model->vert(face[j]);
             screen_coords[j] = world2screen(world_coords[j]);
         }
-        triangleWithTexture(screen_coords,tex_coords,zBuffer,image,texture);
+        //the normal of a face
+        Vec3f n = cross((world_coords[2] - world_coords[0]),(world_coords[1] - world_coords[0]));
+        n.normalize();
+        float intensity = n * light_dir;
+
+        if (intensity > 0)
+            triangle(screen_coords,zBuffer,image,TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+
     }
     image.flip_vertically(); // to place the origin in the bottom left corner of the image
-    image.write_tga_file("flat-shading-with-texture.tga");
+    image.write_tga_file("flat-shading-with-zBuffer.tga");
     delete model;
-//    delete[] zBuffer;
     return 0;
 }
 
